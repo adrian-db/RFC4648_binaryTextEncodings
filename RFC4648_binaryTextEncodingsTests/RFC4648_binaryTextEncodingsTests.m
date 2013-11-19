9f5f0be6780902148e2b674121c97547ff89f7d0
Adrian Bigland <a.d.bigland@gmail.com>
2013-11-19 10:54:47 +0000

    Initial Commit

---
+++ RFC4648_binaryTextEncodingsTests/RFC4648_binaryTextEncodingsTests.m
@@ -0,0 +1,34 @@
+//
+//  RFC4648_binaryTextEncodingsTests.m
+//  RFC4648_binaryTextEncodingsTests
+//
+//  Created by Adrian Bigland on 19/11/2013.
+//  Copyright (c) 2013 Adrian Bigland. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RFC4648_binaryTextEncodingsTests : XCTestCase
+
+@end
+
+@implementation RFC4648_binaryTextEncodingsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
